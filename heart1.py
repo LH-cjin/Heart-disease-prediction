3c05d4e1637547576a6080abd55e418010731aec
LH-cjin <95098783+LH-cjin@users.noreply.github.com>
2025-03-15 14:19:08 +0800

    Update heart1.py

--- heart1.py
+++ heart1.py
@@ -1,16 +1,16 @@
- import streamlit as st
- import pandas as pd
- import numpy as np
- import matplotlib.pyplot as plt
- import seaborn as sns
- from sklearn.model_selection import train_test_split
- from sklearn.linear_model import LogisticRegression
- from sklearn.naive_bayes import GaussianNB
- from sklearn.tree import DecisionTreeClassifier
- from sklearn.ensemble import RandomForestClassifier
- from sklearn.neighbors import KNeighborsClassifier
- from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, roc_curve, auc
- from sklearn.preprocessing import StandardScaler
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, roc_curve, auc
+from sklearn.preprocessing import StandardScaler
  
  # 设置 Streamlit 页面配置
  st.set_page_config(page_title="心脏病预测", page_icon="❤️", layout="wide")
